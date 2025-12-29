7abdadbd1e0bb59791d07b171c65e124f4709213
gabicasas01 <casas.gf7@gmail.com>
2025-12-29 12:13:31 -0300

    refactor: update App to use dynamic event routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import Presentacion from './Presentacion.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/presentacion" element={<Presentacion />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
